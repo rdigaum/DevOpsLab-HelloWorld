32b3dc188ba098421a6e3ddb727730fe9f05d59e
rodrigobrito <rodrigobrito@bsd.com.br>
2019-08-06 22:00:24 -0400

    Update test.py - texto - v1

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "EDevops Essential - Python.")
+        self.assertRegex(result.data.decode(), "Devops Essential - Python.")
 
 
 if __name__ == "__main__":
