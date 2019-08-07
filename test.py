219ea4f08ade5e5374120d4b758c5d32b374fa3e
rodrigobrito <rodrigobrito@bsd.com.br>
2019-08-06 22:52:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Devops Essential - Python.")
+        self.assertRegex(result.data.decode(), "baby Project")
 
 
 if __name__ == "__main__":
